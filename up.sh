f03986de85acd024896442c2fc36cf6dd68355a9
Springcomp <springcomp@users.noreply.github.com>
2023-09-21 22:00:37 +0000

    up.sh

--- up.sh
+++ up.sh
@@ -5,4 +5,4 @@ DOMAIN=$(grep "^DOMAIN" .env | awk -F '=' '{print $2}')
 sed -i -e "s/domain.tld/${DOMAIN}/g" ./acme.sh/www/.well-known/mta-sts.txt
 sed -i -e "s/domain.tld/${DOMAIN}/g" ./nginx/conf.d/default.conf
 
-#docker compose up --detach
+docker compose up --detach
