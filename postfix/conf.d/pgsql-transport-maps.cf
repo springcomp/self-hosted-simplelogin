a9d1e3f6fbc86e75a1f3ec3644efa95e7a5586e7
Springcomp <springcomp@users.noreply.github.com>
2023-09-26 15:59:45 +0000

    postfix

---
+++ postfix/conf.d/pgsql-transport-maps.cf
@@ -0,0 +1,9 @@
+# postgres config
+hosts = sl-db
+user = myuser
+password = mypassword
+dbname = simplelogin
+
+# forward to smtp:sl-email:20381 for custom domain AND email domain
+query = SELECT 'smtp:sl-email20381' FROM custom_domain WHERE domain = '%s' AND verified=true
+    UNION SELECT 'smtp:sl-email:20381' WHERE '%s' = 'domain.tld' LIMIT 1;
--- up.sh
+++ up.sh
@@ -1,8 +1,21 @@
 #!/bin/env bash
 
+ere_quote() {
+  # this escapes regex reserved characters
+  # it also escape / for subsequent use with sed
+  sed 's/[][\/\.|$(){}?+*^]/\\&/g' <<< "$*"
+}
+
 DOMAIN=$(grep "^DOMAIN" .env | awk -F '=' '{print $2}')
+PG_USERNAME=$(grep "^POSTGRES_USER" .env | awk -F '=' '{print $2}')
+PG_PASSWORD=$(grep "^POSTGRES_PASSWORD" .env | awk -F '=' '{print $2}')
 
 sed -i -e "s/domain.tld/${DOMAIN}/g" ./acme.sh/www/.well-known/mta-sts.txt
 sed -i -e "s/domain.tld/${DOMAIN}/g" ./nginx/conf.d/default.conf
+sed -i -e "s/domain.tld/${DOMAIN}/g" ./postfix/conf.d/main.cf
+sed -i -e "s/myuser/${PG_USERNAME}/g" ./postfix/conf.d/pgsql-relay-domains.cf
+sed -i -e "s/mypassword/$(ere_quote ${PG_PASSWORD})/g" ./postfix/conf.d/pgsql-relay-domains.cf
+sed -i -e "s/myuser/${PG_USERNAME}/g" ./postfix/conf.d/pgsql-transport-maps.cf
+sed -i -e "s/mypassword/$(ere_quote ${PG_PASSWORD})/g" ./postfix/conf.d/pgsql-transport-maps.cf
 
 docker compose up --detach
