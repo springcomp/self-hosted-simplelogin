05861dfee8d58ceaabad423313a7e4ae21120807
Maxime Labelle <springcomp@users.noreply.github.com>
2023-09-26 23:06:40 +0200

    Uncomment some template replacements

--- up.sh
+++ up.sh
@@ -2,7 +2,7 @@
 
 ere_quote() {
   # this escapes regex reserved characters
-  # it also escape / for subsequent use with sed
+  # it also escapes / for subsequent use with sed
   sed 's/[][\/\.|$(){}?+*^]/\\&/g' <<< "$*"
 }
 
@@ -10,10 +10,10 @@ DOMAIN=$(grep "^DOMAIN" .env | awk -F '=' '{print $2}')
 PG_USERNAME=$(grep "^POSTGRES_USER" .env | awk -F '=' '{print $2}')
 PG_PASSWORD=$(grep "^POSTGRES_PASSWORD" .env | awk -F '=' '{print $2}')
 
-# sed -e "s/domain.tld/${DOMAIN}/g" ./acme.sh/www/.well-known/mta-sts.txt.tpl >./acme.sh/www/.well-known/mta-sts.txt
-# sed -e "s/domain.tld/${DOMAIN}/g" ./nginx/conf.d/default.conf.tpl > ./nginx/conf.d/default.conf
-# 
-# sed -e "s/domain.tld/${DOMAIN}/g" ./postfix/conf.d/main.cf.tpl > ./postfix/conf.d/main.cf
+sed -e "s/domain.tld/${DOMAIN}/g" ./acme.sh/www/.well-known/mta-sts.txt.tpl >./acme.sh/www/.well-known/mta-sts.txt
+sed -e "s/domain.tld/${DOMAIN}/g" ./nginx/conf.d/default.conf.tpl > ./nginx/conf.d/default.conf
+
+sed -e "s/domain.tld/${DOMAIN}/g" ./postfix/conf.d/main.cf.tpl > ./postfix/conf.d/main.cf
 
 sed -e "s/myuser/${PG_USERNAME}/g" ./postfix/conf.d/pgsql-relay-domains.cf.tpl >./postfix/conf.d/pgsql-relay-domains.cf
 sed -i -e "s/mypassword/$(ere_quote ${PG_PASSWORD})/g" ./postfix/conf.d/pgsql-relay-domains.cf
