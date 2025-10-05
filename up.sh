4190fe6e29e0f652f66b09062755781e1f675258
chrisblech <chrisblech@users.noreply.github.com>
2025-10-05 17:32:07 +0000

    Simplify Traefik konfiguration (now only in .env), bugfix certificate-exporter config

--- up.sh
+++ up.sh
@@ -9,7 +9,7 @@ ere_quote() {
 has_wildcard_san() {
   cert="$1"    # Pfad zu .crt oder fullchain
   domain="$2"  # z.B. domain.tld
-  openssl x509 -in "$cert" -noout -text 2>/dev/null | grep "DNS:*.$domain" >/dev/null
+  openssl x509 -in "$cert" -noout -text 2>/dev/null | grep -E -q 'DNS:[[:space:]]*\*\.'"$domain"'(,|$)' >/dev/null
 }
 
 DOMAIN=$(grep "^DOMAIN" .env | awk -F '=' '{print $2}')
@@ -17,17 +17,22 @@ SUBDOMAIN=$(grep "^SUBDOMAIN" .env | awk -F '=' '{print $2}')
 PG_USERNAME=$(grep "^POSTGRES_USER" .env | awk -F '=' '{print $2}')
 PG_PASSWORD=$(grep "^POSTGRES_PASSWORD" .env | awk -F '=' '{print $2}')
 
+if [ -z "$DOMAIN" ]; then
+  echo "ERROR: ENV Var DOMAIN must be set!"
+  exit 1
+fi
+
 if [ -z "$SUBDOMAIN" ]; then
   SUBDOMAIN="app"
 fi
 
 sed -e "s/app.domain.tld/${SUBDOMAIN}.${DOMAIN}/g" -e "s/domain.tld/${DOMAIN}/g" ./postfix/conf.d/main.cf.tpl > ./postfix/conf.d/main.cf
 
-CERT_SUB="/certs/${SUBDOMAIN}.domain.tld.fullchain.pem"
-CERT_DOMAIN="/certs/domain.tld.fullchain.pem"
+CERT_SUB="/certs/${SUBDOMAIN}.${DOMAIN}"
+CERT_DOMAIN="/certs/${DOMAIN}"
 
-if [ -s $CERT_DOMAIN ] && ( [ ! -s $CERT_SUB ] || has_wildcard_san $CERT_DOMAIN $DOMAIN); then
-  sed -i -e "s/${CERT_SUB}/${CERT_DOMAIN}/g" ./postfix/conf.d/main.cf
+if [ -s $CERT_DOMAIN.fullchain.pem ] && ( [ ! -s $CERT_SUB.fullchain.pem ] || has_wildcard_san $CERT_DOMAIN.fullchain.pem $DOMAIN); then
+  sed -i -e "s,${CERT_SUB},${CERT_DOMAIN},g" ./postfix/conf.d/main.cf
 fi
 
 if [ ! -f ./postfix/conf.d/virtual ]; then
